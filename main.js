69712e4a51b068faa9bb27bc04c5cc7080663b2c
mwas19 <157672231+mwas19@users.noreply.github.com>
2026-03-27 16:34:12 +0300

    Step 6: Created contacts table with action buttons

---
+++ main.js
@@ -0,0 +1,11 @@
+function viewDetails() {
+    alert("Viewing employee details");
+}
+
+function editContact() {
+    alert("Edit feature coming soon");
+}
+
+function deleteContact() {
+    confirm("Are you sure you want to delete?");
+}
